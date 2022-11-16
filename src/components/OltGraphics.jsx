b352f08fdb94303812178e3e5324fd0f006e80ce
Abdulhafeez Kekere-Ekun <abiolahafeez@yahoo.com>
2022-11-16 17:08:04 +0100

    further adjustments to OLT

--- src/components/OltGraphics.jsx
+++ src/components/OltGraphics.jsx
@@ -3,35 +3,33 @@ import { Images } from '../Icons&Images';
 
 const OltGraphics = () => {
     return (
-            <div className='container '>
-                <div className="row gx-1 bg-white pt-4 p-b-71 olt-container">
-                    <div className="col-lg-3 col-sm-12">
-                        <img src={Images.graphic} alt="" className='img-fluid' />
-                    </div>
-                    <div className="col-lg-9 col-sm-12 text-cyan olt-comp">
-                        <div className="p-t-10 ">
-                            <h4 className='f-w-600 f-s-22 olt-h4'>Graphics Design</h4>
-                            <p className='f-s-14 p-t-10 olt-p'>
-                                You’ll participate in a wide-range of design projects where you’ll gain hands-on experience with
-                                the fundamentals of design methodology. You will gain industry knowledge on how to create
-                                illustrations, edit images using tools like Adobe illustrator, photoshop and Corel Draw
-                            </p>
-                            <div className="row m-t-30 olt-comp-3">
-                                <div className="col-md-2">
-                                    <img src={Images.cal} alt="" width={21} className="img-fluid m-b-5" /> <span className='f-s-14 '>10 Weeks</span>
-                                </div>
-                                <div className="col-md-4">
-                                    <img src={Images.IconClock} alt="" width={21} className="img-fluid m-b-5" /> <span className='f-s-14'>Starting August 2021</span>
-                                </div>
-                            </div>
-                            <div className="row m-t-40 olt-comp-4">
-                                <div className="col-md-6 text-md-start"><h4 className='f-w-700 f-s-20'>&#8358;150,000.00</h4></div>
-                                <div className="col-md-6 text-md-end olt-comp-4-b"><button type="button" class="btn btn-another-blue text-white btn-enroll f-s-16">Enroll</button></div>
-                            </div>
+        <div className='container'>
+            <div className="row pt-7 bg-white pb-20 olt-container">
+                <div className="col-lg-3 col-sm-12 olt-img">
+                    <img src={Images.ui} alt="" className='olt-img-ui' />
+                </div>
+                <div className="col-lg-9 col-sm-12 text-cyan olt-comp ps-10">
+                    <h4 className='f-w-600 f-s-22 olt-h4'>Graphics Design</h4>
+                    <p className='f-s-14 p-t-10 olt-p olt-p mt-3 text-cyan'>
+                        You’ll participate in a wide-range of design projects where you’ll gain hands-on experience with
+                        the fundamentals of design methodology. You will gain industry knowledge on how to create
+                        illustrations, edit images using tools like Adobe illustrator, photoshop and Corel Draw
+                    </p>
+                    <div className="row mt-6 olt-comp-3">
+                        <div className="col-xl-2 col-md-6 text-md-start">
+                            <img src={Images.cal} alt="" width={21} className="img-fluid mb-2" /> <span >10 Weeks</span>
                         </div>
+                        <div className="col-xl-4 col-md-6 ">
+                            <img src={Images.IconClock} alt="" width={21} className="img-fluid mb-1" /> <span >Starting August 2021</span>
+                        </div>
+                    </div>
+                    <div className="row m-t-40 olt-comp-4">
+                        <div className="col-md-6 text-md-start"><h4 className='fw-bold fs-20 mt-4'>&#8358;150,000.00</h4></div>
+                        <div className="col-md-6 text-md-end olt-comp-4-b "><button type="button" class="btn btn-another-blue text-white btn-enroll">Enroll</button></div>
                     </div>
                 </div>
             </div>
+        </div>
     );
 }
 
--- src/components/OltUiUx.jsx
+++ src/components/OltUiUx.jsx
@@ -4,30 +4,28 @@ import { Images } from '../Icons&Images';
 const OltUiUx = () => {
     return (
         <div className='container'>
-            <div className="row gx-1 bg-white pt-4 p-b-71 olt-container">
-                <div className="col-lg-3 col-sm-12">
-                    <img src={Images.ui} alt="" className='img-fluid' />
+            <div className="row pt-7 bg-white pb-20 olt-container">
+                <div className="col-lg-3 col-sm-12 olt-img">
+                    <img src={Images.ui} alt="" className='olt-img-ui' />
                 </div>
-                <div className="col-lg-9 col-sm-12 text-cyan olt-comp">
-                    <div className="p-t-10 ">
-                        <h4 className='f-w-600 f-s-22 olt-h4'>User Interface and User Experience Design</h4>
-                        <p className='f-s-14 p-t-10 olt-p olt-p'>
-                            You’ll participate in a wide-range of design projects where you’ll gain hands-on experience with
-                            the fundamentals of design methodology. You’ll identify the problems, develop solutions, and
-                            create prototypes and wireframes using standard design tools including Figma and Balsamiq.
-                        </p>
-                        <div className="row m-t-30 olt-comp-3">
-                            <div className="col-md-2 text-center">
-                                <img src={Images.cal} alt="" width={21} className="img-fluid m-b-5" /> <span className='f-s-14 '>10 Weeks</span>
-                            </div>
-                            <div className="col-md-4">
-                                <img src={Images.IconClock} alt="" width={21} className="img-fluid m-b-5" /> <span className='f-s-14'>Starting August 2021</span>
-                            </div>
+                <div className="col-lg-9 col-sm-12 text-cyan olt-comp ps-10">
+                    <h4 className='f-w-600 fs-22 olt-h4'>User Interface and User Experience Design</h4>
+                    <p className='f-s-14 p-t-10 olt-p olt-p mt-3 text-cyan'>
+                        You’ll participate in a wide-range of design projects where you’ll gain hands-on experience with
+                        the fundamentals of design methodology. You’ll identify the problems, develop solutions, and
+                        create prototypes and wireframes using standard design tools including Figma and Balsamiq.
+                    </p>
+                    <div className="row mt-6 olt-comp-3">
+                        <div className="col-xl-2 col-md-6 text-md-start">
+                            <img src={Images.cal} alt="" width={21} className="img-fluid mb-2" /> <span className=''>10 Weeks</span>
                         </div>
-                        <div className="row m-t-40 olt-comp-4">
-                            <div className="col-md-6 text-md-start"><h4 className='f-w-700 f-s-20'>&#8358;150,000.00</h4></div>
-                            <div className="col-md-6 text-md-end olt-comp-4-b"><button type="button" class="btn btn-another-blue text-white btn-enroll f-s-16">Enroll</button></div>
+                        <div className="col-xl-4 col-md-6">
+                            <img src={Images.IconClock} alt="" width={21} className="img-fluid mb-1" /> <span className=''>Starting August 2021</span>
                         </div>
+                    </div>
+                    <div className="row mt-10 olt-comp-4">
+                        <div className="col-md-6 text-md-start"><h4 className='fs-20 fw-bold mt-4'>&#8358;150,000.00</h4></div>
+                        <div className="col-md-6 text-md-end olt-comp-4-b"><button type="button" class="btn btn-another-blue text-white btn-enroll">Enroll</button></div>
                     </div>
                 </div>
             </div>
