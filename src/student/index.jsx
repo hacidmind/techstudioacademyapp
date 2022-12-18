b92d40a1d1259f361ef9f67f4442c5f5a64d884d
Tobi Olanitori <tobiolanitori@gmail.com>
2022-12-18 07:07:48 +0100

    STYLE: adjusted sidebar jsx

--- src/student/index.jsx
+++ src/student/index.jsx
@@ -6,10 +6,10 @@ import Sidebar from "./sidebar";
 const StudentLayout = () => {
   return (
     <div className="app__flex-3">
-      <div style={{width: '6%', height: '100%'}}>
+      <div style={{ width: "6%", height: "100%" }}>
         <Sidebar />
       </div>
-      <div style={{width: '94%', height: '100%'}}>
+      <div style={{ width: "94%", height: "100%" }}>
         <Outlet />
       </div>
     </div>
